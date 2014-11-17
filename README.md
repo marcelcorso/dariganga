2310ab18508b07189796b5bf3ebbae08db1d7a92
Marcel Corso <marcel.corso@gmail.com>
2014-11-17 16:15:57 +0100

    Сайн байна уу дэлхий

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Dariganga (Mongolian: Дарьганга) is a sum (district) of Sükhbaatar Province in eastern Mongolia.
+
+[Dariganga,_Sükhbaata on wikipedia](http://en.wikipedia.org/wiki/Dariganga,_S%C3%BCkhbaatar)
+
+![Shiliin Bogd Mountain](https://c1.staticflickr.com/1/65/228732950_5a66aafa02_z.jpg?zz=1 "Shiliin Bogd Mountain")
+
---
+++ daringanga.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "github.com/marcelcorso/nuruu"
+)
+
+func IN(h *nuruu.Graph) []string {
+
+  marked := make(map[string]bool)
+
+  for _, v := range(h.Vertices()) {
+    if h.Degree(v) == 0 {
+      marked[v] = true
+    } else {
+      if (rand.Float64() < (1 / h.Degree(v))) {
+        marked[v] = true
+      }
+    }
+  }
+
+  for _, edge := range(h.Edges()) {
+    if marked[edge.row] && edge.column[edge.column] {
+      if rand.Float64() < (h.Degree(edge.column) / (h.Degree(edge.row + h.Degree(edge.column))) ) {
+        delete(marked, edge.row)
+      } else {
+        delete(marked, edge.column)
+      }
+    }
+  }
+
+  // S = set of vertices that remain marked
+  vertices := make([]string, 0, len(marked))
+  for key := range marked {
+    vertices = append(vertices, key)
+  }
+  return vertices
+}
+
+func main() {
+
+  h := nuruu.NewGraph()
+  h.Add("a", "b")
+
+  i := nuruu.NewGraph()
+  for ; len(h.Vertices()) > 0; {
+    s := IndependentSet(h)
+    h.RemoveVertices(s.Vertices() + s.Neighbors(s.Vertices()))
+
+    i.AddGraph(s)
+  }
+
+  // i has the independent set
+
+  fmt.Println("Hello, dude")
+}
+
